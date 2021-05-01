91b78c1714ad7dcc35409f0c48ca37c9d7b21f9c
matheus lopes <matheuslopes.developer@gmail.com>
2021-05-01 18:02:35 -0300

    making first query

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql" if Rails.env.development?
   post "/graphql", to: "graphql#execute"
+  root 'library#index'
 end
